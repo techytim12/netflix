ac2ad895e93acbf31d6f37494ac44960f7cbb1b2
Thomas Philip <thomasphilipchry@gmail.com>
2021-06-22 00:45:32 +0530

    structure done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
 import React from 'react';
 import NavBar from './Components/NavBar/NavBar';
+import Banner from './Components/Banner/Banner';
+import RowPost from './Components/RowPost/RowPost';
+
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <Banner/>
+      <RowPost />
     </div>
   );
 }
--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import './Banner.css'
 
 function Banner() {
     return (
         <div className="banner">
-            
+            <div className="content">
+                <h1 className="title">Movie Name</h1>
+                <div className="banner_buttons">
+                    <button className="button">Play</button>
+                    <button className="button">My List</button>
+                </div>
+                <h1 className="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis modi qui labore nesciunt tempora corporis</h1>
+            </div>
+
+            <div className="fade_bottom">
+                
+            </div>
         </div>
     )
 }
