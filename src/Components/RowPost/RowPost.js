8fbdc481806d1b6e3e5534bb242537a31b107d74
Thomas Philip <thomasphilipchry@gmail.com>
2021-06-22 14:07:20 +0530

    integrated movie trailers

--- src/Components/RowPost/RowPost.js
+++ src/Components/RowPost/RowPost.js
@@ -1,11 +1,13 @@
 import React, {useEffect, useState } from 'react'
-import { imageUrl } from '../../constants/constants'
+import { imageUrl, API_KEY } from '../../constants/constants'
 import './RowPost.css'
 import axios from '../../axios'
+import YouTube from 'react-youtube'
 
 function RowPost(props) {
 
     const [movies, setMovies] = useState([])
+    const [urlId, setUrlId ] = useState('')
 
     useEffect(() => {
         axios.get(props.url).then((response) => {
@@ -16,18 +18,39 @@ function RowPost(props) {
         })
     }, [])
 
+    const opts = {
+        height: '390',
+        width: '100%',
+        playerVars: {
+          // https://developers.google.com/youtube/player_parameters
+          autoplay: 1,
+        },
+      };
+
+      const handleMovie = (id) => {
+        console.log(id);
+        axios.get(`/movie/${id}/videos?api_key=${API_KEY}&language=en-US`).then((response) => {
+            /* console.log(response.data); */
+            if (response.data.results.length != 0){
+                setUrlId(response.data.results[0])
+            }
+            else {
+                console.log(('Trailer not available'));
+            }
+        })
+      }
+
     return (
         <div className="row">
             <h2>{props.title}</h2>
             <div className="posters">
                 {movies.map((obj) => {
                    return (
-                    <img className={ props.isSmall ? 'smallPoster' : 'poster'} src={`${imageUrl+obj.backdrop_path}`} alt="poster" />
+                    <img onClick={() => handleMovie(obj.id) } className={ props.isSmall ? 'smallPoster' : 'poster'} src={`${imageUrl+obj.backdrop_path}`} alt="poster" />
                    ) 
                 })}
-                
-                
             </div>
+            { urlId && <YouTube opts={opts} videoId={urlId.key}  /> }
         </div>
     )
 }
